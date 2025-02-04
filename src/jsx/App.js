e4bc1a734b64235c5f2f4bded63e494debc22329
RAMYA-1994 <ramyarrr2016@gmail.com>
2025-02-04 15:46:35 +0530

    update

--- src/jsx/App.js
+++ src/jsx/App.js
@@ -1,35 +1,33 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import '../css/App.css'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import '../css/App.css';
 
 import LoadLoginPage from "./Login.jsx";
 import Contact from "./Contact.jsx";
 import About from "./About.jsx";
 import Dashboard from "./Dashboard.jsx";
-import LoadWelcome from './Welcome.jsx'
-import LoadRegisterPage from './Register.jsx'
+import LoadWelcome from "./Welcome.jsx";
+import LoadRegisterPage from "./Register.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <nav className="NavBar">
-        <Link className = 'home' to="/">Home</Link>
-
-        <Link className = 'home' to="/dashboard">Dashboard</Link>
-        <Link className = 'home' to="/about">About</Link>
-        <Link className = 'home' to="/contact">Contact</Link>
-
+        <Link className="home" to="/">Home</Link>
+        <Link className="home" to="/dashboard">Dashboard</Link>
+        <Link className="home" to="/about">About</Link>
+        <Link className="home" to="/contact">Contact</Link>
       </nav>
 
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/" component={LoadWelcome} />
-
-        <Route exact path="/login" component={LoadLoginPage} />
-        <Route path="/register" component={LoadRegisterPage} />
-
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-
+      {/* Wrap all Routes inside a <Routes> component */}
+      <Routes>
+        <Route path="/" element={<LoadWelcome />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<LoadLoginPage />} />
+        <Route path="/register" element={<LoadRegisterPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </BrowserRouter>
   );
 }
